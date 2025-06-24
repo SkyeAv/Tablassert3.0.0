990b49fe088c30d43e7f8d49a2350c1932bc11f2
SkyeAv <skye.a.goetz@gmail.com>
2025-06-24 10:45:58 -0700

    Skye Lane Goetz (commiting so I can turn off my computer) Tue Jun 24 10:45:58 PDT 2025

--- tablassert/src/tablassert/core/dataframing.py
+++ tablassert/src/tablassert/core/dataframing.py
@@ -1,22 +1,53 @@
 from tablassert.src.tablassert.models.table_config import Section, Location, PdfHyperparameters, CsvHyperparameters, ExcelHyperparameters
 from tablassert.src.tablassert.models.io import PydanticModel
+from typing import Optional
 from pathlib import Path
 import polars as pl
+
+def take_rows_dataframe(df: pl.DataFrame, rows: set[int]) -> pl.DataFrame:
+    return df.select(pl.all().take(indices=list(rows)))  # type: ignore
+
+def single_bounded_slice_dataframe(df: pl.DataFrame, start: Optional[int], end: Optional[int]) -> pl.DataFrame:
+    # slice requires both a start and end... to use python slices you need this
+    single_bounded_slice: slice = slice(start, end)
+    return pl.DataFrame(df.rows()[single_bounded_slice], schema=df.schema) 
+
+def slice_dataframe(df: pl.DataFrame, start: int, end: int) -> pl.DataFrame:
+    slice_length: int = end - start
+    return df.slice(offset=start, length=slice_length)  # this is faster than single_bounded_slice
+
+def dataframe_preprocessing(df: pl.DataFrame, start: Optional[int], end: Optional[int], rows: Optional[set[int]]) -> pl.DataFrame:
+    if (start and end):
+        return slice_dataframe(df, start, end)
+    elif (start or end):
+        return single_bounded_slice_dataframe(df, start, end)
+    elif rows:
+        return take_rows_dataframe(df, rows)
+    raise RuntimeError("Either start/end or eows must be specified")
 
 EXCEL_ENGINE: str = "calamine"
 
 def read_excel(DownloadHyperparameters: ExcelHyperparameters, datapath: Path) -> pl.DataFrame:
-    DownloadHyperparameters.
-    return pl.read_excel(
-        soruce=datapath,
-        sheet_name=
+    sheetname: str = DownloadHyperparameters.which_excel_sheet_to_use
+    start: Optional[int] = DownloadHyperparameters.start_at_line_number
+    end: Optional[int] = DownloadHyperparameters.end_at_line_number
+    rows: Optional[set[int]] = DownloadHyperparameters.use_row_numbers
+    df: pl.DataFrame = pl.read_excel(
+        source=datapath,
+        sheet_name=sheetname,
         engine=EXCEL_ENGINE,
-    )
+    )  # type: ignore
+    return dataframe_preprocessing(df, start, end, rows)
 
 def read_csv(DownloadHyperparameters: CsvHyperparameters, datapath: Path) -> pl.DataFrame:
-    return pl.read_csv(
-        soruce=datapath,
+    delimiter: str = DownloadHyperparameters.file_delimiter
+    start: Optional[int] = DownloadHyperparameters.start_at_line_number
+    end: Optional[int] = DownloadHyperparameters.end_at_line_number
+    rows: Optional[set[int]] = DownloadHyperparameters.use_row_numbers
+    df: pl.DataFrame = pl.read_csv(
+        source=datapath,
     )
+    return dataframe_preprocessing(df, start, end, rows)
 
 # add support later (not needed ASAP)
 #def read_pdf(DownloadHyperparameters: PdfHyperparameters, datapath: Path) -> pl.DataFrame:
@@ -28,6 +59,7 @@ def invoke(TableLocation: Location, datapath: Path) -> pl.DataFrame:
         return read_excel(DownloadHyperparameters, datapath)
     elif isinstance(DownloadHyperparameters, CsvHyperparameters):    
         return read_csv(DownloadHyperparameters, datapath)
+    raise RuntimeError("Only xls, xlsx, csv, tsv, txt, and pdf are accepted")
     # add support later (not needed ASAP)
     #elif isinstance(DownloadHyperparameters, PdfHyperparameters):   
         #return pl.DataFrame()   
@@ -35,3 +67,4 @@ def invoke(TableLocation: Location, datapath: Path) -> pl.DataFrame:
 def dataframing(Table: Section, datapath: Path) -> pl.DataFrame:
     TableLocation: Location = Table.location
     df: pl.DataFrame = invoke(TableLocation, datapath)
+    return df
