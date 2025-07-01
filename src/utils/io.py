ddb80e427be489fef88b496a6c7d0727169da406
SkyeAv <skye.a.goetz@gmail.com>
2025-07-01 10:27:24 -0700

    Skye Lane Goetz (refactored and improved typesafe IO) Tue Jul  1 10:27:24 AM PDT 2025

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,24 +1,71 @@
+from pydantic import ValidationError, BaseModel, DirectoryPath
+from src.models.table import TableConfig, Section
+from src.models.graph import GraphConfig
+from ruamel.yaml.error import YAMLError
+from typing import Any, Type, TypeVar
 from functools import lru_cache
 from ruamel.yaml import YAML
 from pathlib import Path
 
+
 @lru_cache(maxsize=1)
-def project_root(io_utility_path: Path = Path(__file__).resolve(), configuration_at_root: str = "pyproject.toml") -> Path:
+def project_root(
+    io_utility_path: Path = Path(__file__).resolve(),
+    configuration_at_root: str = "pyproject.toml",
+) -> Path:
     for parent in [io_utility_path] + list(io_utility_path.parents):
         if (parent / configuration_at_root).exists():
             return parent.resolve()
-    raise FileNotFoundError(f"CODE:110 | Couldn't locate {configuration_at_root} in IO utility parents")
+    raise FileNotFoundError(
+        f"CODE:110 | Couldn't locate {configuration_at_root} in IO utility parents"
+    )
 
-# ruamel.yaml
+
+# from ruamel.yaml
 yaml = YAML()
+
 
 def load_yaml(filename: Path) -> Any:
     try:
         with filename.open("r") as f:
             return yaml.load(f)
     except FileNotFoundError:
-        raise RuntimeError(filename.as_posix() + " not found")
+        raise RuntimeError(f"CODE:111 | {filename.as_posix()} not found")
     except PermissionError:
-        raise RuntimeError("Permission denied: " + filename.as_posix())
+        raise RuntimeError(f"CODE:112 | Permission denied: {filename.as_posix()}")
     except YAMLError as e:
-        raise RuntimeError("YAML parsing error in " + filename.as_posix() + " " + str(e))
+        raise RuntimeError(
+            f"CODE:113 | YAML parsing error in {filename.as_posix()} {str(e)}"
+        )
+
+
+PydanticModel = TypeVar("PydanticModel", bound=BaseModel)
+
+
+# checks for validations errors
+def load_model(parsed_yaml: Any, model: Type[PydanticModel]) -> PydanticModel:
+    try:
+        return model.model_validate(parsed_yaml)
+    except ValidationError as e:
+        raise RuntimeError(f"CODE:114 | {model.__name__}: {str(e)}")
+
+
+TABLE_CONFIG_EXTENSION: str = ".yaml"
+
+
+def get_sections(
+    directories: list[DirectoryPath], Graph: GraphConfig
+) -> list[tuple[Section, GraphConfig, int]]:
+    sections: list[tuple[Section, GraphConfig, int]] = []
+    for d in directories:
+        for path in Path(str(d)).rglob("*"):
+            if path.suffix.lower() == TABLE_CONFIG_EXTENSION:
+                table_yaml: Any = load_yaml(path)
+                Table: TableConfig = load_model(table_yaml, TableConfig)
+                model = Table.model_dump()
+                subsections = model.get("sections", [])
+                if subsections:
+                    for idx, section in enumerate(subsections, start=1):
+                        SubSection: Section = load_model(section, Section)
+                        sections.append((SubSection, Graph, idx))
+    return sections
