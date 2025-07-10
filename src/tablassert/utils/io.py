11cc397ffc6c6c333e0184ca101aa791f4f6c230
SkyeAv <skye.a.goetz@gmail.com>
2025-07-10 09:37:27 -0700

    Skye Lane Goetz (updated the header to be better for 2025) Thu Jul 10 09:37:27 AM PDT 2025

--- src/tablassert/utils/io.py
+++ src/tablassert/utils/io.py
@@ -104,7 +104,7 @@ def download(link: str, storagepath: Path) -> Path:
     if not filepath.exists():
 
         try:
-            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.112 Safari/537.36"
             headers = {"User-Agent": user_agent}
             resp = requests.get(
                 link, headers=headers, stream=True, timeout=30
