b7ed71e72f113e13508444922ab0b5da8fde7976
SkyeAv <skye.a.goetz@gmail.com>
2025-08-12 16:52:05 -0700

    Skye Lane Goetz (changing stuff for nix) Tue Aug 12 04:52:05 PM PDT 2025

--- flake.nix
+++ flake.nix
@@ -8,15 +8,11 @@
       url = "github:SkyeAv/Tablassert/4.3.0";
       flake = false;
     };
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-    };
   };
   outputs = {
     self,
     nixpkgs,
     tablassert,
-    poetry2nix,
   }: let
     systems = [
       "x86_64-linux"
@@ -30,9 +26,6 @@
           inherit system;
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [
-              poetry2nix.overlay
-            ];
           };
           lib = nixpkgs.lib;
         });
@@ -42,16 +35,72 @@
       lib,
       system,
     }: let
-      python = pkgs.python313;
       py = pkgs.python313Packages;
-    in {
-      myapp = pkgs.poetry2nix.mkPoetryApplication {
-        projectDir = tablassert;
-        python = pkgs.python313;
+      enCoreWebSm = py.buildPythonPackage rec {
+        pname = "en-core-web-sm";
+        version = "3.8.0";
+        format = "wheel";
+        src = pkgs.fetchurl {
+          url = "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-${version}/en_core_web_sm-${version}-py3-none-any.whl";
+          sha256 = "sha256-GTJCnbcn1L/z3u1rNM/AXfF3lPSlLusmz4ko98Gg+4U=";
+        };
+        doCheck = false;
       };
-      python-env = pkgs.poetry2nix.mkPoetryEnv {
-        projectDir = tablassert;
-        python = pkgs.python313;
+      torchDr = py.buildPythonPackage rec {
+        pname = "torchdr";
+        version = "0.3";
+        src = pkgs.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-AAA"; # let fail and copy hash later
+        };
+        doCheck = false;
+      };
+      bmt = py.buildPythonPackage rec {
+        pname = "bmt";
+        version = "1.4.5";
+        src = pkgs.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-AAA"; # let fail and copy hash later
+        };
+        doCheck = false;
+      };
+    in {
+      myapp = py.buildPythonApplication {
+        pname = "tablassert";
+        version = "4.3.0";
+        src = tablassert;
+        pyproject = true;
+        build-system = with py; [
+          setuptools
+          wheel
+        ];
+        propagatedBuildInputs = with py; [
+          pydantic
+          ruamel-yaml
+          sqlite-utils
+          diskcache
+          loguru
+          spacy
+          polars-lts-cpu
+          xlsx2csv
+          typer
+          deepmerge
+          pyarrow
+          requests
+          openpyxl
+          xlrd
+          pandas
+          torch
+          transformers
+          scikit-learn
+          numpy
+          joblib
+          torchDr
+          bmt
+          matplotlib
+          playwright
+          enCoreWebSm
+        ];
       };
       default = self.packages.${system}.myapp;
     });
@@ -62,7 +111,7 @@
     }: {
       default = pkgs.mkShell {
         packages = [
-          self.packages.${system}.python-env
+          self.packages.${system}.myapp-env
           pkgs.git
           pkgs.pkg-config
           pkgs.playwright-core
