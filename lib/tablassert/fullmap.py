752934f98586bd942818736ee0cce0340a349d5b
SkyeAv <skye.lane.goetz@gmail.com>
2026-01-28 15:02:14 -0800

    feat: add distinct terms extraction helper

--- lib/tablassert/fullmap.py
+++ lib/tablassert/fullmap.py
@@ -5,6 +5,17 @@ from operator import add
 import polars as pl
 import duckdb
 
+def distinct(df: pl.DataFrame, l0: str, l1: str) -> pl.DataFrame:
+  # ? Extract Unique Terms From Two Text Normalization Columns
+  t0: pl.DataFrame = df.select(pl.col(l0).alias("term")).unique()
+  t0 = t0.with_columns(pl.lit(0).alias("nlp level"))
+
+  t1: pl.DataFrame = df.select(pl.col(l1).alias("term")).unique()
+  t1 = t1.with_columns(pl.lit(1).alias("nlp level"))
+
+  terms: pl.DataFrame = pl.concat([t0, t1]).unique(subset=["term"])
+  return terms.with_row_index("term id")
+
 def version4(
   p: Path,
   col: str,
