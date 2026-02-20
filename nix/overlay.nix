5ddbc9fa8f15ce83cf7436f07514d7ada459eefc
SkyeAv <skye.lane.goetz@gmail.com>
2026-02-20 11:55:48 -0800

    Updated Versions

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -29,7 +29,7 @@ final: prev: {
       };
       tablassert = pyFinal.buildPythonApplication rec {
         pname = "tablassert";
-        version = "6.1.0";
+        version = "6.2.0";
         format = "pyproject";
         src = ../.;
         build-system = with pyFinal; [
