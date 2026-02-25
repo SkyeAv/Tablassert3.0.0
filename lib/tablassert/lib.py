e55adf51409c3076b631e80ec178ede20bf7198f
SkyeAv <skye.lane.goetz@gmail.com>
2026-02-24 18:29:54 -0800

    Updated Progress Bar Text

--- lib/tablassert/lib.py
+++ lib/tablassert/lib.py
@@ -444,13 +444,13 @@ def build_knowledge_graph(
 
   with progress:
     # ? Load Tables
-    t1: Any = progress.add_task("Loading tables...", total=None)
+    t1: Any = progress.add_task("Loading Tables...", total=None)
     with Pool() as pool:
       raw: list[object] = pool.map(from_yaml, g.tables)
     progress.update(t1, total=1, completed=1)
 
     # ? Extract Sections
-    t2: Any = progress.add_task("Extracting sections...", total=None)
+    t2: Any = progress.add_task("Extracting Sections...", total=None)
     with Pool() as pool:
       temp: list[list[dict[str, Any]]] = pool.map(to_sections, raw)  # pyright: ignore
     progress.update(t2, total=1, completed=1)
@@ -458,19 +458,19 @@ def build_knowledge_graph(
     n: int = len(sections)
 
     # ? Build Tcodes
-    t3: Any = progress.add_task("Building tcodes...", total=n)
+    t3: Any = progress.add_task("Building TCode...", total=n)
     tcode: list[Tcode] = [Tcode.model_validate({**s, "number": idx, "store": (STORE / f"{mkhash(s)}.parquet")}) for idx, s in track(progress, t3, enumerate(sections, start=1))]
     with duckdb.connect(g.dbssert, read_only=True) as conn:
       # ? Collect Instructions
-      t4: Any = progress.add_task("Collecting instructions...", total=n)
+      t4: Any = progress.add_task("Collecting Instructions...", total=n)
       instructions: list[Union[list[tuple[Callable, tuple[Any, ...]]], Path]] = [x.collect(conn, g.pubmed_db, g.pmc_db) for x in track(progress, t4, tcode)]  # pyright: ignore
 
       # ? Build Subgraphs
-      t5: Any = progress.add_task("Building subgraphs...", total=n)
+      t5: Any = progress.add_task("Building Subgraphs...", total=n)
       subgraphs: list[Path] = [op if isinstance(op, Path) else compile_subgraph(op) for op in track(progress, t5, instructions)]  # pyright: ignore
 
     # ? Compile Graph
-    t6: Any = progress.add_task("Compiling graph...", total=None)
+    t6: Any = progress.add_task("Compiling Graph...", total=None)
     compile_graph(subgraphs, g.name, g.version)
     progress.update(t6, total=1, completed=1)
 
