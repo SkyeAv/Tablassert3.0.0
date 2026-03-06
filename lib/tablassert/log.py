9487958d6e96d9b60321a6e5e90e266843a1d785
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-06 11:44:09 -0800

    Made Code More Readable

--- lib/tablassert/log.py
+++ lib/tablassert/log.py
@@ -5,4 +5,4 @@ LOGASSERT: Path = Path("./logassert")
 LOGASSERT.mkdir(parents=True, exist_ok=True)
 
 logger.remove()
-logger.add((LOGASSERT / "logassert.log"), level="INFO", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", rotation="100 MB", encoding="utf-8",)
+logger.add((LOGASSERT / "logassert.log"), level="INFO", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", rotation="100 MB", encoding="utf-8")
