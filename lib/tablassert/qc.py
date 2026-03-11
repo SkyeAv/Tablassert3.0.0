699ebea25b1f9ebcdaef78e640d7b21ea199e20c
SkyeAv <skye.a.goetz@gmail.com>
2026-03-10 19:13:47 -0700

    Fixed QC Logging

--- lib/tablassert/qc.py
+++ lib/tablassert/qc.py
@@ -71,7 +71,7 @@ def fullmap_audit(lf: pl.LazyFrame, col: str, section_hash: str, config_file: st
   passed: pl.DataFrame = pairs.filter(pl.col(out))
   pending: pl.DataFrame = pairs.filter(~pl.col(out))
 
-  exempt_curies: str = r"^CHEBI|^PR|^UniProtKB|"
+  exempt_curies: str = r"^CHEBI|^PR|^UniProtKB"
   is_exempt: pl.DataFrame = pending.with_columns(pl.col(cols[0]).str.contains(exempt_curies).alias(out))
   pairs = pl.concat((passed, is_exempt))
 
