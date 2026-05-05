4cdfcd8023dac33caf8f1ab1e5f7fdd0e79e8c2c
SkyeAv <skye.a.goetz@gmail.com>
2026-05-05 15:10:58 -0700

    Fixed Log

--- src/tablassert/log.py
+++ src/tablassert/log.py
@@ -16,7 +16,7 @@ LOG_FORMAT: str = "{time:YYYY-MM-DD HH:mm:ss} | {level} | {extra[category]} | {m
 logger.configure(extra={"category": "PIPELINE"})
 logger.remove()
 logger.add(
-    (LOGASSERT / "tablassert.log"), level="INFO", format=LOG_FORMAT, rotation="100 MB", encoding="utf-8", mode="w"
+    (LOGASSERT / "logassert.log"), level="INFO", format=LOG_FORMAT, rotation="100 MB", encoding="utf-8", mode="w"
 )
 
 
